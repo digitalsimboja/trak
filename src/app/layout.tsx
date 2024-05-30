69291db33e23c60b54c029f87f96e55c87fc86ac
Sunday Mgbogu <digitalsimboja@gmail.com>
2024-05-30 10:52:38 +0100

    Update the sidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+        <div className="flex justify-between max-w-6xl mx-auto">
+          <div className="hidden sm:inline border-r h-screen"><Sidebar /></div>
+          <div>{children}</div>
+          <div>News</div>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,7 @@
+const PostPage: React.FC = () => {
+    return (
+        <div>Post ID</div>
+    )
+}
+
+export default PostPage;
